688c59080d9809cf2ee2dc92ab60bebffb8dac7c
justinphan55 <phanhuytinuelk10404b@gmail.com>
2024-04-05 17:51:32 +0700

    change location backup

--- stockwarehouse/settings.py
+++ stockwarehouse/settings.py
@@ -204,7 +204,7 @@ DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
 DBBACKUP_CLEANUP_KEEP = True
 DBBACKUP_CLEANUP_KEEP_NUMBER = 3  # Số lượng bản sao lưu giữ lại
 DBBACKUP_STORAGE_OPTIONS = {
-    'location': '/root/eco_warehouse/backup/', 
+    'location': '/root/eco_service/warehouse/backup/', 
 }
 
 
